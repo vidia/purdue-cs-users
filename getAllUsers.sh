c9648426eb6e06e330171a8e5156697560afaae8
David Tschida <dmtschida1@gmail.com>
2014-04-24 00:48:23 -0400

    Gets all users and their host and terminal into a single file

--- getAllUsers.sh
+++ getAllUsers.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-rm ~/users.txt
+rm ~/users.log
+
 
 echo "Deploying command '$@'..."
 
 for HOST in moore{00..24} sslab{00..24} borg{00..24} xinu{00..21} sac{01..13} pod{1..5}-{1..5} pod0-0 mc{01..18}; do
     echo $HOST
-    ssh -o StrictHostKeyChecking=no `whoami`@${HOST}.cs.purdue.edu "hostname -s >> ~/users.txt && echo --begin users-- >> ~/users.txt && who >> ~/users.txt && echo --end host-- >> ~/users.txt"  >>/tmp/deploy_test 2>&1
+# who > who.txt && sed -i 's/^/data /' who.txt && awk '{ print $1,$2,$3 }' who.txt >> users.log 
+    ssh -o StrictHostKeyChecking=no `whoami`@${HOST}.cs.purdue.edu "who > who.txt && sed -i 's/^/${HOST} /' who.txt && awk '{ print \$1,\$2,\$3 }' who.txt >> ~/users.log"  >>/tmp/deploy_test 2>&1
 done
 
 echo "Done."
